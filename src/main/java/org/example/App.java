369c57165a22df2dd312d0db2c4e3e20043cd39f
tsiti <t.sith96@gmail.com>
2022-01-05 15:37:26 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try{
+        Parent parent = FXMLLoader.load(App.class.getResource("/App.fxml"));
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+        stage.setMinWidth(1003);
+        stage.setMinHeight(650);
+        } catch (java.io.IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+public class Controller implements Initializable{
+
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void close() {
+        System.exit(0);
+    }
+}
