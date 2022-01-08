fc6f5a10819d89a52708404dc0c6ed05eb3d3166
yeet <t.sith96@gmail.com>
2022-01-08 12:26:46 +0100

    Revert "fucking workws"
    This reverts commit 7c6854a0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,5 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires javafx.swing;
     exports org.example;
 }
--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.awt.*;
+import javafx.scene.image.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,22 +14,14 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.stage.Window;
-
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import static javafx.embed.swing.SwingFXUtils.fromFXImage;
 
 public class Controller implements Initializable{
 
@@ -54,6 +53,7 @@ public class Controller implements Initializable{
         selectedFile = fileChooser.showOpenDialog(mainWindow);
 
         image = new Image("file:" + selectedFile.getAbsolutePath(), true);
+
         imageView.setImage(image);
     }
 
@@ -66,7 +66,7 @@ public class Controller implements Initializable{
         fileChooser.getExtensionFilters().add(extFilter);
         try {
             File file = fileChooser.showSaveDialog(mainWindow);
-            BufferedImage bImage = ImageIO.read((ImageInputStream) image);
+            BufferedImage bImage = ImageIO.read((ImageInputStream) imageView.getImage());
             if (bImage != null) {
                 ImageIO.write(bImage, "png", file);
             }
@@ -85,7 +85,6 @@ public class Controller implements Initializable{
         }
 
             imageView.setImage(getJavaFXImage(bImage));
-            image = getJavaFXImage(bImage);
     }
 
     public Image getJavaFXImage(BufferedImage bImage) {
@@ -100,25 +99,24 @@ public class Controller implements Initializable{
         return wr;
     }
 
+
     public void applyNegativeFilter() {
-        try {
-            BufferedImage currentImage = null;
-            currentImage = fromFXImage(image, null);
-            for (int x = 0;x<currentImage.getWidth();x++) {
-                for (int y = 0;y<currentImage.getHeight();y++) {
-                    int rgb = currentImage.getRGB(x,y);
-                    Color col = new Color(rgb, false);
-                    col = new Color(255 - col.getRed(),
-                            255 - col.getGreen(),
-                            255 - col.getBlue());
-                    currentImage.setRGB(x, y, col.getRGB());
-                }
+        Image currentImage = imageView.getImage();
+
+        BufferedImage bufferedImage = new BufferedImage((int)currentImage.getWidth(), (int)currentImage.getHeight(), BufferedImage.TYPE_INT_BGR);
+        for (int x = 0;x<bufferedImage.getWidth();x++) {
+            for (int y = 0;y<bufferedImage.getWidth();y++) {
+                int rgb = bufferedImage.getRGB(x,y);
+                Color col = new Color(rgb, false);
+                System.out.println(col.getBlue() + col.getGreen() + col.getRed());
+                col = new Color(255 - col.getRed(),
+                        255 - col.getGreen(),
+                        255 - col.getBlue());
+                System.out.println(col.getBlue() + col.getGreen() + col.getRed());
+                bufferedImage.setRGB(x, y, col.getRGB());
             }
-            imageView.setImage(getJavaFXImage(currentImage));
-            image = getJavaFXImage(currentImage);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
         }
+        imageView.setImage(getJavaFXImage(bufferedImage));
     }
 
 
